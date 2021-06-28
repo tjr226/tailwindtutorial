f8e2e73387a013c8ff73c39ff8ebe688c0e2ce3c
tjr226 <tjr226@gmail.com>
2021-06-28 15:18:17 -0400

    tutorial complete

--- tailwindcss-react-v1-yt/src/App.js
+++ tailwindcss-react-v1-yt/src/App.js
@@ -1,13 +1,50 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Home from './pages/index.js';
+import { Switch, Route } from 'react-router-dom';
+import About from './pages/about.js';
+import Menu from './pages/menu';
+import Dropdown from './components/Dropdown';
+import Contact from './pages/contact';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if(window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+      }
+    }
+
+    window.addEventListener('resize', hideMenu)
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    }
+  })
+
+
+
   return (
     <>
-      <Navbar />
-      <Hero /> 
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/menu' component={Menu} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+
+      <Footer />
+
     </>
   );
 }
--- tailwindcss-react-v1-yt/src/components/Navbar.js
+++ tailwindcss-react-v1-yt/src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
     return (
         <nav className="flex justify-between item-center h-16 bg-white text-black relative shadow-sm font-monos" role="navigation">
             <Link to='/' className="pl-8">EGG</Link>
-            <div className="px-4 cursor-pointer md:hidden">
+            <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
